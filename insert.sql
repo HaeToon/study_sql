3b5bcddc0022461fe418c1b45ac9c3fc0c7846e8
HaeToon <gxg4205@gmail.com>
2024-08-05 16:50:11 +0900

    fixed error

--- insert.sql
+++ insert.sql
@@ -1,16 +1,14 @@
-INSERT INTO `NOTICE` (`NOTICE_PK`, `CONTENT`, `WRITER`) VALUES
+INSERT INTO `WRITER` (`WRITER_PK`,`WRITER_NAME`) VALUES
+('WRITER_01', '관리자'),
+('WRITER_02', '마케팅팀'),
+('WRITER_03', '운영팀');
+INSERT INTO `NOTICE` (`NOTICE_PK`, `CONTENT`, `WRITER_FK`) VALUES
 ('NOTICE_01', '서비스 점검 안내', 'WRITER_01'),
 ('NOTICE_02', '이벤트 안내', 'WRITER_02'),
-('NOTICE_03', '새로운 기능 추가 안내', 'WRITER_03'),
-('NOTICE_04', '결제 시스템 업그레이드 안내', 'WRITER_04'),
-('NOTICE_05', '서비스 이용약관 변경 안내', 'WRITER_05');
-INSERT INTO `WRITER` (`WRITER_PK`, `NOTICE_FK`, `WRITER_NAME`) VALUES
-('WRITER_01', 'NOTICE_01', '관리자'),
-('WRITER_02', 'NOTICE_02', '마케팅팀'),
-('WRITER_03', 'NOTICE_03', '관리자'),
-('WRITER_04', 'NOTICE_04', '운영팀'),
-('WRITER_05', 'NOTICE_05', '운영팀');
-INSERT INTO `VISITORS` (`VISITORS_PK`, `NOTICE_FK`, `VISITOR_NAME`) VALUES
+('NOTICE_03', '새로운 기능 추가 안내', 'WRITER_01'),
+('NOTICE_04', '결제 시스템 업그레이드 안내', 'WRITER_03'),
+('NOTICE_05', '서비스 이용약관 변경 안내', 'WRITER_03');
+INSERT INTO `VISITOR` (`VISITOR_PK`, `NOTICE_FK`, `VISITOR_NAME`) VALUES
 ('VI_01', 'NOTICE_01', '홍길동'),
 ('VI_02', 'NOTICE_01', '김영희'),
 ('VI_03', 'NOTICE_02', '백지영'),
