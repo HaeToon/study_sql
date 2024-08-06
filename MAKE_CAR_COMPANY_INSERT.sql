511540fcfe83f11565a624fa7b665bbacdc8ad01
HaeToon <gxg4205@gmail.com>
2024-08-06 10:52:29 +0900

    n:n

--- MAKE_CAR_COMPANY_INSERT.sql
+++ MAKE_CAR_COMPANY_INSERT.sql
@@ -10,14 +10,25 @@ INSERT INTO CAR (CAR_PK, CAR_NAME, YEARS, COMPANY_FK) VALUES
 ('CAR_03', '카마로', '2019', 'COMPANY_03'),
 ('CAR_04', '3시리즈', '2017', 'COMPANY_04'),
 ('CAR_05', 'E클래스', '2021', 'COMPANY_05');
-INSERT INTO CAR_OPTION (CAR_OPTION_PK, COMPANY_FK, CAR_FK) VALUES
-('네비게이션','COMPANY_01', 'CAR_01'),
-('후방카메라','COMPANY_01', 'CAR_01'),
-('가죽시트','COMPANY_02', 'CAR_02'),
-('스마트키','COMPANY_02', 'CAR_02'),
-('크루즈컨트롤','COMPANY_03', 'CAR_03'),
-('블루투스','COMPANY_03', 'CAR_03'),
-('후방감지센서','COMPANY_04', 'CAR_04'),
-('헤드업디스플레이','COMPANY_04', 'CAR_04'),
-('전자식메모리시트','COMPANY_05', 'CAR_05'),
-('어댑티브크루즈컨트롤','COMPANY_05', 'CAR_05');
+INSERT INTO OPTION_TABLE VALUES
+('OPTION_01','네비게이션'),
+('OPTION_02','후방카메라'),
+('OPTION_03','가죽시트'),
+('OPTION_04','스마트키'),
+('OPTION_05','크루즈컨트롤'),
+('OPTION_06','블루투스'),
+('OPTION_07','후방감지센서'),
+('OPTION_08','헤드업디스플레이'),
+('OPTION_09','전자식메모리시트'),
+('OPTION_10','어댑티브크루즈컨트롤');
+INSERT INTO CAR_OPTION (CAR_FK, OPTION_FK) VALUES
+('CAR_01','OPTION_01'),
+('CAR_01','OPTION_02'),
+('CAR_02','OPTION_03'),
+('CAR_02','OPTION_04'),
+('CAR_03','OPTION_05'),
+('CAR_03','OPTION_06'),
+('CAR_04','OPTION_07'),
+('CAR_04','OPTION_08'),
+('CAR_05','OPTION_09'),
+('CAR_05','OPTION_10');
